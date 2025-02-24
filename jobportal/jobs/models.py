f3ca4dc9d981ceac0745d76bc4fea8021e4ac7d5
Ashik_Ridwan <ridwanashik004@gmail.com>
2025-02-24 09:13:14 +0600

    solved model mistake title

--- jobportal/jobs/models.py
+++ jobportal/jobs/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Job(models.Model):
-    tittle = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
     location = models.CharField(max_length=100)
     description = models.TextField()
     salary = models.DecimalField(max_digits=10, decimal_places=2)
