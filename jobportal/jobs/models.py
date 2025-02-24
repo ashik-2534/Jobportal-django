2f22a7da07924d96489c176dc2c8b96dbf488524
Ashik_Ridwan <ridwanashik004@gmail.com>
2025-02-24 08:43:15 +0600

    added job model in models

--- jobportal/jobs/models.py
+++ jobportal/jobs/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Job(models.Model):
+    tittle = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    description = models.TextField()
+    salary = models.DecimalField(max_digits=10, decimal_places=2)
+    post_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.tittle
