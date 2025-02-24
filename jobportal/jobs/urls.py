1e2422af3e3465bc1481611718b06b68335b8f82
Ashik_Ridwan <ridwanashik004@gmail.com>
2025-02-24 11:44:44 +0600

    created job apply templates

--- jobportal/jobs/urls.py
+++ jobportal/jobs/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.job_list, name='job_list'),
     path('job/<int:job_id>', views.job_details, name='job_details'),
-    path('apply/<int:job_id>', views.apply_job, name='apply_job'),
+    path('job/<int:job_id>/apply', views.apply_job, name='apply_job'),
 
 ]
