7c973a9aa6e9ea97c0161f4909f478f32b92a30a
Ashik_Ridwan <ridwanashik004@gmail.com>
2025-02-24 08:49:27 +0600

    created job list view

--- jobportal/jobs/views.py
+++ jobportal/jobs/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Job
 
 # Create your views here.
+def job_list(request):
+    jobs = Job.objects.all()
+    return render (request, 'job/job_list.html', {
+        'jobs':jobs
+    })
