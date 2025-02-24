043d204f0d41d5f799fce4f4913a561f82f65ff9
Ashik_Ridwan <ridwanashik004@gmail.com>
2025-02-24 09:08:06 +0600

    set urls in urls and registered models in admin and created superuser

--- jobportal/jobportal/urls.py
+++ jobportal/jobportal/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from jobs import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.job_list, name='job_list'),
 ]
